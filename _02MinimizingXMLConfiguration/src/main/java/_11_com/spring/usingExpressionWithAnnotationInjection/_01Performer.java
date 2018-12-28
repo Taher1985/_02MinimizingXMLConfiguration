d9c0f813862b84f06956a1736a696a8765337d31
Taher <Taher@192.168.1.5>
2018-12-28 18:42:37 +0530

    ApplicationProperties

---
+++ _02MinimizingXMLConfiguration/src/main/java/_11_com/spring/usingExpressionWithAnnotationInjection/_01Performer.java
@@ -0,0 +1,7 @@
+package _11_com.spring.usingExpressionWithAnnotationInjection;
+
+public interface _01Performer {
+
+	void perform();
+
+}
---
+++ _02MinimizingXMLConfiguration/src/main/java/_11_com/spring/usingExpressionWithAnnotationInjection/_02nstrumentalist.java
@@ -0,0 +1,18 @@
+package _11_com.spring.usingExpressionWithAnnotationInjection;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class _02nstrumentalist implements _01Performer {
+
+	@Value("DillDiyaGalla")
+	private String song;
+
+	@Value("${msg}")
+	private String propertyFile;
+
+	public void perform() {
+		System.out.println("Song is " + song + " and messgae is "
+				+ propertyFile);
+	}
+
+}
